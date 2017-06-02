bfeeed30e06d27cf83bbe0f109cba95999a2265d
Eduardo Lago <ed@aleph.engineering>
2017-06-02 17:29:56 -0400

    supporting delete / forget message rest endpoint

--- src/main/scala/sma/cmd/Commands.scala
+++ src/main/scala/sma/cmd/Commands.scala
@@ -2,17 +2,16 @@ package sma.cmd
 
 import akka.actor.ActorRef
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.PathMatcher._
-import akka.http.scaladsl.server.PathMatchers._
+import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.FutureDirectives.onSuccess
-import akka.http.scaladsl.server.directives.MethodDirectives.put
+import akka.http.scaladsl.server.directives.MethodDirectives.{delete, put}
 import akka.http.scaladsl.server.directives.PathDirectives.path
 import akka.http.scaladsl.server.directives.RouteDirectives.complete
 import akka.pattern.ask
 import akka.util.Timeout
-import DiggingMessages.{Follow, FollowReply}
 import sma.EventSourcing
+import sma.cmd.DiggingMessages.{Follow, FollowReply, Forget, ForgetReply}
 
 import scala.concurrent.duration._
 
@@ -31,6 +30,13 @@ trait Commands extends EventSourcing {
           onSuccess(digger ? Follow("user123", interest)) {
             case response: FollowReply =>
               complete(StatusCodes.OK, s"follow ack received!")
+            case _ =>
+              complete(StatusCodes.InternalServerError)
+          }
+        } ~ delete {
+          onSuccess(digger ? Forget("user123", interest)) {
+            case response: ForgetReply =>
+              complete(StatusCodes.OK, s"forget ack received!")
             case _ =>
               complete(StatusCodes.InternalServerError)
           }
