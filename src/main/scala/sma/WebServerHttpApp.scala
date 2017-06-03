65443fa384ef1c2695649230c7059494d7bbc72f
Eduardo Lago <ed@aleph.engineering>
2017-06-02 20:28:45 -0400

    refactoring websever startup

--- src/main/scala/sma/WebServerHttpApp.scala
+++ src/main/scala/sma/WebServerHttpApp.scala
@@ -1,15 +1,12 @@
 package sma
 
-import akka.actor.Props
 import akka.http.scaladsl.server.HttpApp
-import sma.cmd.{Twitter, Facebook, Digger, Commands}
+import sma.cmd._
 import sma.qry.{Profile, Queries}
 
 object WebServerHttpApp extends HttpApp with App with Commands with Queries {
 
-  implicit val twitter = system.actorOf(Twitter.props(), "twitter")
-  implicit val facebook = system.actorOf(Facebook.props(), "facebook")
-  implicit val digger = system.actorOf(Props(new Digger(twitter, facebook)), "digger")
+  implicit val digger = system.actorOf(Digger.props(), "digger")
 
   implicit val profile = system.actorOf(Profile.props(), "profile")
 
