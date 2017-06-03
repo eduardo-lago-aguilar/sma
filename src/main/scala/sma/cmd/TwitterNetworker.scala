bff6c60b059e705de1645a3b228e4eab0c147ac9
Eduardo Lago <ed@aleph.engineering>
2017-06-03 18:18:04 -0400

    fully refactoring twitter networker & reactive wrapper actor

--- src/main/scala/sma/cmd/TwitterNetworker.scala
+++ src/main/scala/sma/cmd/TwitterNetworker.scala
@@ -5,14 +5,14 @@ import akka.actor._
 import akka.kafka.Subscriptions
 import akka.kafka.scaladsl.Consumer
 import akka.pattern.ask
-import akka.stream.scaladsl.{Source, Sink}
+import akka.stream.scaladsl.{Sink, Source}
 import sma.Redis.Interests
 import sma.cmd.DiggingMessages._
+import sma.reactive.ReactiveWrappedActor
 import sma.{Committing, Receiving}
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.util.{Failure, Success}
 
 object TwitterNetworker {
   def props(): Props = {
@@ -20,22 +20,25 @@ object TwitterNetworker {
   }
 }
 
-class TwitterNetworker(val topic: String) extends Actor with ActorLogging with Receiving with Committing {
-
-  implicit val timeout = akka.util.Timeout(5 seconds)
-
-  println(s"--> [${self.path.name}] creating actor ")
+class TwitterNetworker(val topic: String) extends ReactiveWrappedActor with Receiving with Committing {
 
   override def receive = {
     case bulk: DiggingBulk =>
       Source(bulk().toVector)
         .runWith(Sink.foreach[Digging](proccess))
       sender() ! DiggingBulkReply()
-      logMessages(bulk.serialize)
+      log.info(s"--> [${self.path.name}] receiving ${bulk.serialize}")
       streamFromTwitter()
-    case _ =>
-      println(s"--> [${self.path.name}] receiving an unknown message")
-      sender() ! Some()
+  }
+
+  override def consume: Future[Done] = consumeAndBatchAsync
+
+  private def consumeAndBatchAsync: Future[Done] = {
+    Consumer.plainSource(consumerSettings, Subscriptions.topics(topic))
+      .map(msg => Digging.deserialize(msg.value()))
+      .groupedWithin(100, 2 seconds)
+      .mapAsync(1)(bulk => self ? DiggingBulk(bulk))
+      .runWith(Sink.ignore)
   }
 
   private def proccess(dig: Digging): Unit = {
@@ -49,31 +52,6 @@ class TwitterNetworker(val topic: String) extends Actor with ActorLogging with R
     Source.fromFuture[Seq[String]](Interests(topic))
       .map(interest => interest.mkString.toUpperCase)
       .runWith(Sink.foreach(tweet => kafkaProducer.send(kafkaProducerRecord(replyTopic(topic), topic, tweet))))
-  }
-
-  override def preStart: Unit = makeItReactive
-
-  override def postStop(): Unit = println(s"--> [${self.path.name}] actor stopped")
-
-  private def logMessages(followees: String): Unit = log.info(s"--> [${self.path.name}] receiving ${followees}")
-
-  private def makeItReactive: Unit = {
-    log.info(s"--> [${self.path.name}] creating twitter stream")
-    consumeAndBatchAsync.onComplete {
-      case Failure(ex) =>
-        log.error(ex, s"--> [${self.path.name}] stream failed, stopping the actor")
-        self ! PoisonPill
-      case Success(_) =>
-        log.info(s"--> [${self.path.name}] gracefully shutdown")
-    }
-  }
-
-  private def consumeAndBatchAsync: Future[Done] = {
-    Consumer.plainSource(consumerSettings, Subscriptions.topics(topic))
-      .map(msg => Digging.deserialize(msg.value()))
-      .groupedWithin(100, 2 seconds)
-      .mapAsync(1)(bulk => self ? DiggingBulk(bulk))
-      .runWith(Sink.ignore)
   }
 }
 
---
+++ src/main/scala/sma/reactive/ReactiveWrappedActor.scala
@@ -0,0 +1,35 @@
+package sma.reactive
+
+import akka.Done
+import akka.actor.{Actor, PoisonPill, ActorLogging}
+import sma.EventSourcing
+
+import scala.concurrent.Future
+import scala.util.{Success, Failure}
+import scala.concurrent.duration._
+
+trait ReactiveWrappedActor extends Actor with ActorLogging with EventSourcing {
+  implicit val timeout = akka.util.Timeout(5 seconds)
+
+  println(s"--> [${self.path.name}] creating actor ")
+
+  override def preStart: Unit = makeItReactive
+  override def postStop(): Unit = println(s"--> [${self.path.name}] actor stopped")
+  override def unhandled(message: Any): Unit = {
+    println(s"--> [${self.path.name}] receiving an unknown message")
+    self ! PoisonPill
+  }
+
+  def makeItReactive: Unit = {
+    log.info(s"--> [${self.path.name}] creating reactive stream")
+    consume.onComplete {
+      case Failure(ex) =>
+        log.error(ex, s"--> [${self.path.name}] stream failed, stopping the actor")
+        self ! PoisonPill
+      case Success(_) =>
+        log.info(s"--> [${self.path.name}] gracefully shutdown")
+    }
+  }
+
+  def consume: Future[Done]
+}
