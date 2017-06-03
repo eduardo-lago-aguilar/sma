debc7f92f7a50e7670619969702a188e8f434f83
Eduardo Lago <ed@aleph.engineering>
2017-06-03 17:02:06 -0400

    refactoring Twitter actor reactivity to kafka

--- src/main/scala/sma/cmd/Twitter.scala
+++ src/main/scala/sma/cmd/Twitter.scala
@@ -1,5 +1,6 @@
 package sma.cmd
 
+import akka.Done
 import akka.actor._
 import akka.kafka.Subscriptions
 import akka.kafka.scaladsl.Consumer
@@ -9,6 +10,7 @@ import sma.Redis.Interests
 import sma.cmd.DiggingMessages._
 import sma.{Committing, Receiving, StreamWrapper}
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
@@ -49,30 +51,29 @@ class Twitter(val topic: String) extends Actor with ActorLogging with Receiving 
       .runWith(Sink.foreach(tweet => kafkaProducer.send(kafkaProducerRecord(replyTopic(topic), topic, tweet))))
   }
 
-  private def logMessages(followees: String): Unit = {
-    log.info(s"--> [${self.path.name}] receiving ${followees}")
-  }
+  override def preStart: Unit = makeItReactive
 
-  override def preStart: Unit = {
-    println(s"--> [${self.path.name}] creating twitter stream")
-    val processingActor = self
-    val done = Consumer.plainSource(consumerSettings, Subscriptions.topics(topic))
-      .map(msg => Digging.deserialize(msg.value()))
-      .groupedWithin(100, 2 seconds)
-      .mapAsync(1)(bulk => processingActor ? DiggingBulk(bulk))
-      .runWith(Sink.ignore)
-    done.onComplete {
+  override def postStop(): Unit = println(s"--> [${self.path.name}] actor stopped"
+
+  private def logMessages(followees: String): Unit = log.info(s"--> [${self.path.name}] receiving ${followees}")
+
+  private def makeItReactive: Unit = {
+    log.info(s"--> [${self.path.name}] creating twitter stream")
+    consumeAndBatchAsync.onComplete {
       case Failure(ex) =>
         log.error(ex, s"--> [${self.path.name}] stream failed, stopping the actor")
         self ! PoisonPill
       case Success(_) =>
         log.info(s"--> [${self.path.name}] gracefully shutdown")
     }
-
   }
 
-  override def postStop(): Unit = {
-    println(s"--> [${self.path.name}] actor stopped")
+  private def consumeAndBatchAsync: Future[Done] = {
+    Consumer.plainSource(consumerSettings, Subscriptions.topics(topic))
+      .map(msg => Digging.deserialize(msg.value()))
+      .groupedWithin(100, 2 seconds)
+      .mapAsync(1)(bulk => self ? DiggingBulk(bulk))
+      .runWith(Sink.ignore)
   }
 }
 
