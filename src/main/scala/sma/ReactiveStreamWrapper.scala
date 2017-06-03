d14b688b7a61d6a910c0223a063abce212594d83
Eduardo Lago <ed@aleph.engineering>
2017-06-03 17:56:37 -0400

    StreamWrapperTwitter becomes more general purpose ReactiveStreamWrapper for any actor willing to react to kafka topics

---
+++ src/main/scala/sma/ReactiveStreamWrapper.scala
@@ -0,0 +1,28 @@
+package sma
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+
+import scala.concurrent.duration._
+
+object ReactiveStreamWrapper {
+  def apply(implicit system: ActorSystem, childName: String, props: Props): ActorRef = {
+    val name = s"supervisor_of_${childName}"
+
+    println(s"--> [${name}] creating supervisor")
+
+    import akka.pattern.{Backoff, BackoffSupervisor}
+
+    val supervisorProps = BackoffSupervisor.props(
+      Backoff.onStop(
+        props,
+        childName = childName,
+        minBackoff = 3 seconds,
+        maxBackoff = 30 seconds,
+        randomFactor = 0.2
+      )
+    )
+    val supervisor = system.actorOf(supervisorProps, name = name)
+
+    supervisor
+  }
+}
