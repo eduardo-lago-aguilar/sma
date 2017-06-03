e76d4fae0afae4891eb4678c8f56c7408fa8f51f
Eduardo Lago <ed@aleph.engineering>
2017-06-02 20:28:14 -0400

    digger now wakeup actor instead of sending message directly to him

--- src/main/scala/sma/cmd/Digger.scala
+++ src/main/scala/sma/cmd/Digger.scala
@@ -1,8 +1,6 @@
 package sma.cmd
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props}
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.utils.Bytes
+import akka.actor.{Actor, ActorLogging, Props}
 import sma.Committing
 import sma.cmd.DiggingMessages._
 
@@ -13,27 +11,22 @@ object Digger {
   }
 }
 
-class Digger(twitter: ActorRef, facebook: ActorRef) extends Actor with ActorLogging with Committing {
-
-  val medias = Map("twitter" -> twitter, "facebook" -> facebook)
+class Digger extends Actor with ActorLogging with Committing {
 
   override def receive = {
-    case dig: Digging =>
-      commit(dig)
-      forward(dig)
+    case message: Digging =>
+      val topic = digTopic(message.follower, message.media)
+      commit(message, topic)
+      wakeup(topic)
 
-      log.info(s"received ${dig.mkString}")
+      log.info(s"--> [${self.path.name}] received ${message.mkString}")
 
-      sender() ! dig.reply
+      sender() ! message.reply
   }
 
-  def commit(message: Digging) = {
-    kafkaProducer.send(kafkaRecord(message, digTopic(message.follower, message.media)))
+  def commit(message: Digging, topic: String) = {
+    kafkaProducer.send(kafkaRecord(message, topic))
   }
 
-  def forward(message: Digging) = {
-    medias(message.media) ! message
-  }
-
-
+  def wakeup(topic: String) = StreamWrapperTwitter.create(system, topic)
 }
