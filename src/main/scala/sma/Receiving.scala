9c7a8664ff59b3390fa708e263fc5edf8db190b3
Eduardo Lago <ed@aleph.engineering>
2017-06-03 02:09:18 -0400

    receiving deserialized messages

--- src/main/scala/sma/Receiving.scala
+++ src/main/scala/sma/Receiving.scala
@@ -2,10 +2,10 @@ package sma
 
 import akka.kafka.ConsumerSettings
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.common.serialization.{BytesDeserializer, StringDeserializer}
+import org.apache.kafka.common.serialization.StringDeserializer
 
 trait Receiving extends EventSourcing {
-  val consumerSettings = ConsumerSettings(system, new StringDeserializer, new BytesDeserializer)
+  val consumerSettings = ConsumerSettings(system, new StringDeserializer, new StringDeserializer)
     .withBootstrapServers("localhost:9092")
     .withGroupId("group1")
     .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
