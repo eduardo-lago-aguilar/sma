74e8b05841fe09c56d72105adbda8a70c9cc596b
Eduardo Lago <ed@aleph.engineering>
2017-06-04 15:16:04 -0400

    twitter feeder changed to receive new tweet formats

--- src/main/scala/sma/cmd/TwitterFeeder.scala
+++ src/main/scala/sma/cmd/TwitterFeeder.scala
@@ -1,39 +1,32 @@
 package sma.cmd
 
 import akka.Done
-import akka.kafka.Subscriptions
-import akka.kafka.scaladsl.Consumer
 import akka.pattern.ask
 import akka.stream.scaladsl.Sink
-import sma.{TweetReply, Tweet, Receiving}
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import sma.reactive.ReactiveWrappedActor
+import sma.{Receiving, Tweet, TweetReply}
 
 import scala.concurrent.Future
-import scala.concurrent.duration._
 
 class TwitterFeeder(topic: String) extends ReactiveWrappedActor with Receiving {
 
   override def receive = {
     case tweet: Tweet =>
-      println(s"receiving tweet: ${tweet.serialize}")
+      log.info(s"--> [${self.path.name}] receiving tweet ${tweet.body}")
       sender() ! TweetReply()
   }
 
   override def consume: Future[Done] = consumeLinearAsync
 
-
   private def consumeLinearAsync: Future[Done] = {
-    Consumer.plainSource(consumerSettings, Subscriptions.topics(topic))
-      .mapAsync(1)(m => self ? Tweet(m.value()))
+    twitterPlainSource(topic)
+      .mapAsync(1)(record => self ? tweet(record))
       .runWith(Sink.ignore)
   }
 
-  private def consumeAndBatchAsync: Future[Done] = {
-    Consumer.plainSource(consumerSettings, Subscriptions.topics(topic))
-      .map(msg => msg.value())
-      .groupedWithin(100, 2 seconds)
-      .mapAsync(1)(bulk => self ? bulk)
-      .runWith(Sink.ignore)
+  private def tweet(record: ConsumerRecord[Array[Byte], Array[Byte]]): Tweet = {
+    Tweet("some_text", Seq("redbee", "linux"), System.currentTimeMillis()) // TODO: deserialize tweet here!
   }
 
 }
