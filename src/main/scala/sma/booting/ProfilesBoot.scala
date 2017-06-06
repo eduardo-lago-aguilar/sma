0eacda9ac82e93f79b5502ccacce76394496155f
Eduardo Lago <ed@aleph.engineering>
2017-06-06 12:21:04 -0400

    refactoring profilers by using a fully qualify name

--- src/main/scala/sma/booting/ProfilesBoot.scala
+++ src/main/scala/sma/booting/ProfilesBoot.scala
@@ -1,24 +1,23 @@
 package sma.booting
 
 import akka.actor.Props
-import akka.stream.scaladsl.Sink._
 import akka.stream.scaladsl.Source
 import akka.stream.scaladsl.Source._
-import sma.eventsourcing.{ProfileActors, EventSourcing}
+import sma.eventsourcing.EventSourcing
 import sma.feeding.Profiling
 import sma.reactive.ReactiveStreamWrapper
 import sma.storing.Redis._
-import sma.twitter.TwitterFeeder
-import sun.java2d.cmm.Profile
 
-trait ProfilesBoot extends EventSourcing with ProfileActors{
+trait ProfilesBoot extends EventSourcing {
   def wakeupProfiles: Unit = {
     fromFuture(smaUsers)
-      .runWith(foreach(users => Source(users.toVector)
-        .runWith(foreach(user => Source(networks.toVector)
-          .runWith(foreach(net => {
-            profiles = profiles + (digTopic(user, net) -> ReactiveStreamWrapper(system, digTopic(user, net), Props(new Profiling(digTopic(user, net)))))
-          }))))))
+      .runForeach(users => Source(users.toVector)
+        .runForeach(user => Source(networks.toVector)
+          .runForeach(net => {
+            val topic: String = digTopic(user, net)
+            val name = s"${topic}_${Profiling.nick}"
+            ReactiveStreamWrapper(system, name, Props(new Profiling(topic)))
+          })))
   }
 
 }
--- src/main/scala/sma/feeding/Profiling.scala
+++ src/main/scala/sma/feeding/Profiling.scala
@@ -2,12 +2,16 @@ package sma.feeding
 
 import sma.digging.{BulkDigging, DiggingReactive}
 
+object Profiling {
+  val nick = "profiler"
+}
+
 class Profiling(topic: String) extends DiggingReactive(topic) {
   override def receive = {
     case bulk: BulkDigging =>
       super.proccess(bulk, true)
   }
 
-  override def consumerGroup = s"${self.path.name}__profiling"
+  override def consumerGroup = s"${self.path.name}_${Profiling.nick}"
 
 }
