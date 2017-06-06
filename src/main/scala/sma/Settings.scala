fd842f1e684efd356984391873a54a07fd374cb5
Eduardo Lago <ed@aleph.engineering>
2017-06-06 16:02:17 -0400

    settings manager

---
+++ src/main/scala/sma/Settings.scala
@@ -0,0 +1,23 @@
+package sma
+
+import java.io.File
+
+import com.twitter.hbc.httpclient.auth.OAuth1
+import com.typesafe.config.ConfigFactory
+
+import scala.util.Properties
+
+object Settings {
+
+  val twitter = ConfigFactory.parseFile(new File("application.conf")).getConfig("twitter")
+
+  def consumerKey = Properties.envOrElse("TWITTER_CONSUMER_KEY", twitter.getString("consumer_key"))
+
+  def consumerSecret = Properties.envOrElse("TWITTER_CONSUMER_SECRET", twitter.getString("consumer_secret"))
+
+  def token = Properties.envOrElse("TWITTER_TOKEN", twitter.getString("token"))
+
+  def tokenSecret = Properties.envOrElse("TWITTER_TOKEN_SECRET", twitter.getString("token_secret"))
+
+  def oAuth1 = new OAuth1(consumerKey, consumerSecret, token, tokenSecret)
+}
