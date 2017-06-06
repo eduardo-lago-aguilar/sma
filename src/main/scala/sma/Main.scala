a1379038ecd36ab3c7868406f8ad7ba7b6f05463
Eduardo Lago <ed@aleph.engineering>
2017-06-06 14:57:29 -0400

    refactoring startup logic w/ shutdown hook

--- src/main/scala/sma/Main.scala
+++ src/main/scala/sma/Main.scala
@@ -1,13 +1,19 @@
 package sma
 
 import akka.actor.ActorRef
-import akka.http.scaladsl.server.HttpApp
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.{IncomingConnection, ServerBinding}
 import akka.http.scaladsl.server.RouteConcatenation._
+import akka.stream.scaladsl.{Sink, Source}
 import sma.booting.{FeedersBoot, NetworkersBoot, ProfilesBoot}
 import sma.digging.Digger
 import sma.http._
 
-object Main extends HttpApp with App with Commands with Queries with NetworkersBoot with FeedersBoot with ProfilesBoot {
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.sys.ShutdownHookThread
+
+object Main extends App with Commands with Queries with NetworkersBoot with FeedersBoot with ProfilesBoot {
 
   implicit val digger: ActorRef = system.actorOf(Digger.props(), "digger")
 
@@ -17,7 +23,40 @@ object Main extends HttpApp with App with Commands with Queries with NetworkersB
 
   wakeupProfiles
 
-  startServer("localhost", 8080)
+  boot
 
   def routes = commandRoutes ~ queryRoutes
+
+  def boot: ShutdownHookThread = {
+    val httpService = Http()
+
+    val HOST = "127.0.0.1"
+    val PORT = 8080
+
+    val serverSource: Source[IncomingConnection, Future[ServerBinding]] =
+      httpService.bind(interface = HOST, PORT)
+
+    val binding: Future[ServerBinding] =
+      serverSource.to(Sink.foreach { connection =>
+        connection handleWith routes
+      }).run()
+
+    println(s"Server is now online at http://$HOST:$PORT\n")
+
+    //shutdown Hook
+
+    scala.sys.addShutdownHook {
+      println("Terminating...")
+      closingApp = true
+      binding
+        .flatMap(_.unbind())
+        .onComplete { _ =>
+          materializer.shutdown()
+          system.terminate()
+        }
+      Await.result(system.whenTerminated, 5 seconds)
+      println("Terminated... Bye")
+    }
+  }
+
 }
