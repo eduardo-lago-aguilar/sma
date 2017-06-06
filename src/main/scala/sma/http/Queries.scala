76f893e636b91c9275431f32c1d82bb531340060
Eduardo Lago <ed@aleph.engineering>
2017-06-06 00:17:30 -0400

    streaming tweets and tracking terms back to ui using Json Akka Spray

--- src/main/scala/sma/http/Queries.scala
+++ src/main/scala/sma/http/Queries.scala
@@ -1,44 +1,48 @@
 package sma.http
 
-import akka.http.scaladsl.model.StatusCodes
+import akka.NotUsed
+import akka.http.scaladsl.common.EntityStreamingSupport
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.PathDirectives.path
 import akka.http.scaladsl.server.directives.RouteDirectives.complete
+import akka.stream.scaladsl.Source
 import akka.util.Timeout
 import sma.eventsourcing.{EventSourcing, ProfileActors}
-import sma.feeding.RetrieveTrackingTerms
 import sma.storing.Redis.{MessagesStore, TrackingTermsStore}
+import sma.twitter.{TrackingTerm, Tweet}
 
-import scala.concurrent.Await
 import scala.concurrent.duration._
 
 trait Queries extends EventSourcing with ProfileActors {
 
+  import sma.json.CustomJsonProtocol._
+
   implicit val timeout = Timeout(5 seconds)
+
+  implicit val jsonStreamingSupport = EntityStreamingSupport.json()
 
   val queryRoutes: Route = {
     path("interests" / Segment) {
       userAtNetwork =>
         get {
-          complete(StatusCodes.OK, trackingTerms(timeout, userAtNetwork).mkString(", "))
+          complete(trackingTerms(userAtNetwork).map(TrackingTerm))
         }
-    } ~ path("board" / Segment) {
-      userAtNetwork =>
+    } ~ path("board" / Segment / Segment ) {
+      (userAtNetwork,hashTrackingTopics) =>
         get {
-          val terms = trackingTerms(timeout, userAtNetwork)
-          val ttt = trackingTermsTopic(replyTopic(digTopic(userAtNetwork)), terms)
-          complete(StatusCodes.OK, board(userAtNetwork).mkString(", "))
+          complete(board(userAtNetwork, hashTrackingTopics).map(s => Tweet(s, Seq(), timestamp)))
         }
     }
 
   }
 
-  def trackingTerms(timeout: Timeout, userAtNetwork: String): Seq[String] = Await.result(TrackingTermsStore(digTopic(userAtNetwork)), timeout.duration).sorted
+  def trackingTerms(userAtNetwork: String): Source[String, NotUsed] = {
+    Source.fromFuture(TrackingTermsStore(digTopic(userAtNetwork))).mapConcat(seq => seq.toStream)
+  }
 
-  def board(userAtNetwork: String) = {
-    val terms = trackingTerms(timeout, userAtNetwork)
-    val ttt = trackingTermsTopic(replyTopic(digTopic(userAtNetwork)), terms)
-    Await.result(MessagesStore(ttt), timeout.duration)
+  def board(userAtNetwork: String, hashTrackingTopics: String) = {
+    val ttt = trackingTermsTopic(replyTopic(digTopic(userAtNetwork)), hashTrackingTopics)
+    Source.fromFuture(MessagesStore(ttt)).mapConcat(seq => seq.toStream)
   }
 }
