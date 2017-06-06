36cd08e8c3a4adeb24fe27adfeb5d9e0df8db4f6
Eduardo Lago <ed@aleph.engineering>
2017-06-06 00:21:19 -0400

    refactoring uses an disitinct consumer group

--- src/main/scala/sma/feeding/Profiling.scala
+++ src/main/scala/sma/feeding/Profiling.scala
@@ -1,21 +1,13 @@
 package sma.feeding
 
-import akka.actor.Props
 import sma.digging.{BulkDigging, DiggingReactive}
-
-object Profiling {
-  def props(): Props = {
-    Props(classOf[Profiling])
-  }
-}
 
 class Profiling(topic: String) extends DiggingReactive(topic) {
   override def receive = {
-    case rtt: RetrieveTrackingTerms =>
-      receiving(rtt.mkString)
-      sender() ! TrackingTerms(trackingTerms.toVector)
     case bulk: BulkDigging =>
       super.proccess(bulk, true)
   }
+
+  override def consumerGroup = s"${self.path.name}__profiling"
 
 }
