a146a37179b4f2600a749a70e4faa5f73527e0ac
Eduardo Lago <ed@aleph.engineering>
2017-06-06 00:13:31 -0400

    Custom Json protocol formatting for delivering messages to UI in stream fashion

---
+++ src/main/scala/sma/json/CustomJsonProtocol.scala
@@ -0,0 +1,10 @@
+package sma.json;
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import sma.twitter.{TrackingTerm, Tweet}
+import spray.json.DefaultJsonProtocol
+
+object CustomJsonProtocol extends SprayJsonSupport with DefaultJsonProtocol {
+    implicit val tweetFormat = jsonFormat3(Tweet.apply)
+    implicit val trackingTermFormat = jsonFormat1(TrackingTerm.apply)
+}
