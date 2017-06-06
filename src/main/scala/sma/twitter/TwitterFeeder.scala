f35260905bb11c8ae76549bae11d95274166ac1c
Eduardo Lago <ed@aleph.engineering>
2017-06-06 00:23:25 -0400

    refactoring the twitter feeder

--- src/main/scala/sma/twitter/TwitterFeeder.scala
+++ src/main/scala/sma/twitter/TwitterFeeder.scala
@@ -3,9 +3,10 @@ package sma.twitter
 import akka.Done
 import akka.pattern.ask
 import akka.stream.scaladsl.Sink
-import sma.json.Json
 import sma.eventsourcing.Receiving
+import sma.json.Json
 import sma.reactive.ReactiveWrappedActor
+import sma.storing.Redis.MessagesStore
 
 import scala.concurrent.Future
 
@@ -13,14 +14,17 @@ class TwitterFeeder(topic: String) extends ReactiveWrappedActor with Receiving {
 
   override def receive = {
     case tweet: Tweet =>
-      log.info(s"--> [${self.path.name}] receiving tweet ${tweet.body}")
+      val ttt: String = trackingTermsTopic(topic, tweet.trackingTerms)
+      log.info(s"--> [${self.path.name}] receiving tweet ${tweet.body} -> storing message at redis ${ttt}")
+      MessagesStore.add(ttt, tweet.body)
       sender() ! TweetReply()
   }
 
   override def consume: Future[Done] = consumeLinearAsync
 
   private def consumeLinearAsync: Future[Done] = {
-    plainSource(topic)
+    val consumerGroup = s"${self.path.name}__twitter_feeder"
+    plainSource(topic, consumerGroup)
       .mapAsync(1)(record => self ? tweet(record))
       .runWith(Sink.ignore)
   }
