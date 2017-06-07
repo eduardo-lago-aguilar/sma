8668fb61611b9d8973882fa30e142f9292e52236
Eduardo Lago <ed@aleph.engineering>
2017-06-07 19:40:04 -0400

    declaring controller and binding sample data

--- static/sma.js
+++ static/sma.js
@@ -1,13 +1,24 @@
 (function () {
+
     function config($stateProvider) {
         var st = $stateProvider.state;
 
         st("home", {
                 url: "",
-                templateUrl: "home.html"
+                templateUrl: "home.html",
+                controller: 'HomeController',
+                controllerAs: 'hc'
             }
         );
     }
 
     angular.module("sma", ["ui.router"]).config(["$stateProvider", config])
+
+
+    function HomeController() {
+        this.data = "Hellooooo!";
+    }
+
+    angular.module("sma").controller("HomeController", [HomeController]);
+
 })();
