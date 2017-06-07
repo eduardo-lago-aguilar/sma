467a660ccbb152c89e054ba688c98b2026187676
Eduardo Lago <ed@aleph.engineering>
2017-06-07 18:09:59 -0400

    serving static content for a given fixed user@twitter

--- src/main/scala/sma/http/Static.scala
+++ src/main/scala/sma/http/Static.scala
@@ -1,15 +1,21 @@
 package sma.http
 
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.directives.FileAndResourceDirectives.getFromFile
+import akka.http.scaladsl.server.directives.PathDirectives.path
+import akka.http.scaladsl.server.Directives.redirect
+import akka.http.scaladsl.server.directives.PathDirectives.pathSingleSlash
+import akka.http.scaladsl.model.StatusCodes.MovedPermanently
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server._
-import akka.http.scaladsl.server.directives.PathDirectives._
 
 trait Static {
 
   val staticRoutes: Route = {
-    path("/index.html") {
+    pathSingleSlash {
+      redirect("ed@twitter", MovedPermanently)
+    } ~ path("ed@twitter") {
       getFromFile("index.html")
     }
-
   }
+
 }
