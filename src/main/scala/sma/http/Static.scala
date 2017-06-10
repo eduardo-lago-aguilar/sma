9508e2845017ffea4d119c69dc59027559628401
Eduardo Lago <ed@aleph.engineering>
2017-06-10 13:34:55 -0400

    refactoring static routes

--- src/main/scala/sma/http/Static.scala
+++ src/main/scala/sma/http/Static.scala
@@ -2,23 +2,25 @@ package sma.http
 
 import akka.http.scaladsl.model.StatusCodes.MovedPermanently
 import akka.http.scaladsl.server.Directives.{redirect, _}
-import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.FileAndResourceDirectives.getFromFile
 import akka.http.scaladsl.server.directives.PathDirectives.{path, pathSingleSlash}
 import sma.Settings.{defaultNetwork, defaultUser}
 
 trait Static {
 
-  val staticRoutes: Route = {
-    pathSingleSlash {
+  val staticRoutes = {
+    val rootRoute = pathSingleSlash {
       redirect(s"${defaultUser}@${defaultNetwork}", MovedPermanently)
-    } ~ pathPrefix("root") {
-      getFromDirectory("static/root/")
-    } ~ path(Segment) {
+    }
+    val indexRoute = path(Segment) {
       _ => {
         getFromFile("static/root/sma/index.html")
       }
     }
+    val assetsRoute = pathPrefix("root") {
+      getFromDirectory("static/root/")
+    }
+    rootRoute ~ indexRoute ~ assetsRoute
   }
 
 }
