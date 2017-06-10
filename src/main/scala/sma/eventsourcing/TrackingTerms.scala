1ead222c781d16f04da080a6c7a2d90fa84b9512
Eduardo Lago <ed@aleph.engineering>
2017-06-10 14:20:49 -0400

    tracking terms is part of tweets, extracted TweetJsonHelper

--- src/main/scala/sma/eventsourcing/TrackingTerms.scala
+++
@@ -1,5 +0,0 @@
-package sma.eventsourcing
-
-case class TrackingTerms(terms: Seq[String]) {
-  def mkString = terms.mkString(", ")
-}
--- src/main/scala/sma/twitter/Tweet.scala
+++ src/main/scala/sma/twitter/Tweet.scala
@@ -1,25 +1,9 @@
 package sma.twitter
 
-import org.apache.kafka.clients.producer.ProducerRecord
-import sma.eventsourcing.{Receiving, TrackingTerms}
-import sma.json.Json
-
 case class TrackingTerm(term: String)
+
+case class TrackingTerms(terms: Seq[String])
 
 case class Tweet(id: String, body: String, trackingTerms: Seq[String], hashTrackingTerms: String)
 
 case class TweetReply()
-
-object TweetJsonHelper extends Receiving {
-
-  def decodeId(json: String): Option[Any] = Json.decode[Map[String, Any]](json).get("id_str")
-
-  def decodeTweet(record: ConsumerRecordType): Tweet = Json.decode[Tweet](record.value())
-
-  def twitterProducerRecord(tweet: Tweet, targetTopic: String) = {
-    val key = Json.encode(TrackingTerms(tweet.trackingTerms))
-    val value = Json.encode(tweet)
-    new ProducerRecord[Array[Byte], Array[Byte]](targetTopic, key, value)
-  }
-
-}
